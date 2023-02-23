cfaded53dd878961f4242a43dc8c07fcb3339fd6
Funboy650 <123374367+Funboy650users.noreply.github.com>
2023-02-23 12:07:41 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check main
+ * @a: an integer input
+ * @b: an integer input
+ * Return: an integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
