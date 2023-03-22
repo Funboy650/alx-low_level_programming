b501df7e5edaf8c6d95767ca2128ee40c6704838
Funboy650 <123374367+Funboy650users.noreply.github.com>
2023-03-22 13:17:55 -0700

    Most hackers are young because young people tend to be adaptable

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,50 +1,44 @@
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
- * 
+ *
+ * Description: A program that prints the opcodes
+ *              of its own main function.
+ *              Usage: ./main number_of_bytes
+ *
  * @argc: argument counter
  * @argv: argument vector
  *
  * Return: Always Successful
 */
-void print_opcodes(char *a, int n)
+
+int main(int argc, char *argv[])
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: number of arguments passed to the function
- * @argv: array of pointers to arguments
- *
- * Return: always O
- */
-int main(int argc, char **argv)
-{
-	int n;
+	int index, nbytes;
+	char *ptr = (char *) main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	n = atoi(argv[1]);
-	if (n < 0)
+
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	print_opcodes((char *)&main, n);
+
+	for (index = 0; index < nbytes; index++)
+	{
+		printf("%02x", ptr[index] & 0xFF);
+		if (index != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 	return (0);
 }
